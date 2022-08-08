a084f17ae27146eb7760d3417b59a3b6dd34b295
pavloyashchenko <pyashchenko@hallowelt.com>
2022-08-08 18:08:13 +0200

    Replace deprecated overrides of 'AbstractContent::fillParserOutput'
    ERM28899
    
    [4.2.x]
    
    Change-Id: I649067c55ef02ce8a27898946440ce2e857d722f

--- src/ContentHandler/FormDefinitionHandler.php
+++ src/ContentHandler/FormDefinitionHandler.php
@@ -2,10 +2,14 @@
 
 namespace MediaWiki\Extension\Forms\ContentHandler;
 
+use Content;
 use JavaScriptContentHandler;
+use MediaWiki\Content\Renderer\ContentParseParams;
 use MediaWiki\Extension\Forms\Action\FormDefinitionEditAction;
 use MediaWiki\Extension\Forms\Action\FormDefinitionSourceEditAction;
 use MediaWiki\Extension\Forms\Content\FormDefinitionContent;
+use ParserOutput;
+use Title;
 
 class FormDefinitionHandler extends JavaScriptContentHandler {
 	/**
@@ -51,5 +55,46 @@ class FormDefinitionHandler extends JavaScriptContentHandler {
 			'edit' => FormDefinitionEditAction::class,
 			'editdefinitionsource' => FormDefinitionSourceEditAction::class
 		];
+	}
+
+	/**
+	 * @param Content $content
+	 * @param ContentParseParams $cpoParams
+	 * @param ParserOutput &$output The output object to fill (reference).
+	 */
+	protected function fillParserOutput(
+		Content $content,
+		ContentParseParams $cpoParams,
+		ParserOutput &$output
+	) {
+		$dbKey = $cpoParams->getPage()->getDBkey();
+		$title = Title::newFromDBkey( $dbKey );
+		$this->currentForm = $this->getTitleWithoutExtension( $title );
+		$data = (array)$content->getData()->getValue();
+		$this->addCategoriesFromJSON( $output, $data );
+
+		$formDataHandler = new FormDataHandler();
+		$formDataHandler->fillParserOutput( $content, $cpoParams, $output );
+	}
+
+	/**
+	 * @param Title $title
+	 * @return string
+	 */
+	public function getTitleWithoutExtension( $title ) {
+		$prefixedDBKey = $title->getPrefixedDBKey();
+		return substr( $prefixedDBKey, 0, -5 );
+	}
+
+	/**
+	 * @param ParserOutput $output
+	 * @param array $data
+	 * @return void
+	 */
+	private function addCategoriesFromJSON( $output, $data ) {
+		$categories = isset( $data['categories'] ) ? $data['categories'] : [];
+		foreach ( $categories as $categoryName ) {
+			$output->addCategory( $categoryName, $categoryName );
+		}
 	}
 }
