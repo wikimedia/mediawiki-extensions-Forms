0bd09494fd9d0093c813ef2f893a2a8a72240d0f
hslater <hamish.slater@hallowelt.com>
2024-04-09 16:19:24 +1200

    Convert to abstract schema
    ERM36733
    
    [REL1_39, REL1_39-2.0.x, master]
    
    Change-Id: If6b083a6c66ddf318943ba7b56981b7f0af0c9f6
    (cherry picked from commit a700db29993f52fd65c16283aaa6520735314f77)
    (cherry picked from commit 0af93fa3cafacadb54224430da46734be700ecb6)

--- maintenance/db/form_autosave.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/form_autosave (
-	fa_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	fa_data BLOB NULL,
-	fa_timestamp varbinary(15) NOT NULL
-)/*$wgDBTableOptions*/;
