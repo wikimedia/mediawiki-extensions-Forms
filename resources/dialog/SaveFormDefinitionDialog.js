3205550d701111d04a7a2c8c3f8ec8844eaf18e6
d.savuljesku <d.savuljesku@gmail.com>
2024-08-02 16:11:54 +0200

    Use toolbar for buttons in FormEditor
    ... and other improvements
    
    [4.5.x]
    
    ERM30824
    
    Change-Id: I07c4de962711700486e92eea301006687ec43a97

---
+++ resources/dialog/SaveFormDefinitionDialog.js
@@ -0,0 +1,103 @@
+mw.ext.forms.dialog = mw.ext.forms.dialog || {};
+mw.ext.forms.dialog.SaveFormDefinitionDialog = function( cfg ) {
+	mw.ext.forms.dialog.SaveFormDefinitionDialog.parent.call( this, cfg );
+	this.formData = cfg.formData || {};
+	this.itemsForm = cfg.itemsForm || null;
+	this.propertiesForm = cfg.propertiesForm || null;
+	this.targetPage = cfg.targetPage || null;
+};
+
+OO.inheritClass( mw.ext.forms.dialog.SaveFormDefinitionDialog, OO.ui.ProcessDialog );
+
+mw.ext.forms.dialog.SaveFormDefinitionDialog.static.name = 'saveFormDefinitionDialog';
+mw.ext.forms.dialog.SaveFormDefinitionDialog.static.title = mw.message( 'forms-editor-toolbar-save' ).text();
+mw.ext.forms.dialog.SaveFormDefinitionDialog.static.padded = true;
+mw.ext.forms.dialog.SaveFormDefinitionDialog.static.actions = [
+	{ action: 'save', label: mw.message( 'forms-editor-toolbar-done' ).text(), flags: [ 'primary', 'progressive' ] },
+	{ action: 'cancel', label: mw.message( 'forms-editor-toolbar-cancel' ).text(), flags: 'safe' }
+];
+
+mw.ext.forms.dialog.SaveFormDefinitionDialog.prototype.initialize = function () {
+	mw.ext.forms.dialog.SaveFormDefinitionDialog.super.prototype.initialize.call( this );
+
+	this.content = new OO.ui.PanelLayout( {
+		padded: true,
+		expanded: false
+	} );
+	this.$body.append( this.content.$element );
+
+	this.summary = new OO.ui.TextInputWidget( {
+		placeholder: mw.message( 'forms-form-editor-form-props-summary-placeholder' ).text()
+	} );
+	this.content.$element.append( new OO.ui.FieldLayout( this.summary, {
+		label: mw.message( 'forms-form-editor-form-props-summary-label' ).text(),
+		align: 'top'
+	} ).$element );
+};
+
+mw.ext.forms.dialog.SaveFormDefinitionDialog.prototype.getActionProcess = function ( action ) {
+	return mw.ext.forms.dialog.SaveFormDefinitionDialog.parent.prototype.getActionProcess.call( this, action ).next(
+		function() {
+			if ( action === 'save' ) {
+
+				this.pushPending();
+				var mainPromise = $.Deferred(),
+					propertiesPromise = $.Deferred(),
+					itemsPromise = $.Deferred();
+				this.propertiesForm.connect( this, {
+					dataSubmitted: function( data ) {
+						propertiesPromise.resolve( data );
+					},
+					validationFailed: function() {
+						mainPromise.reject();
+					}
+				} );
+				this.propertiesForm.submit();
+
+				this.itemsForm.connect( this, {
+					dataSubmitted: function( data ) {
+						itemsPromise.resolve( data );
+					},
+					validationFailed: function() {
+						mainPromise.reject();
+					}
+				} );
+				this.itemsForm.submit();
+
+				$.when( propertiesPromise, itemsPromise ).done( function( properties, items ) {
+					var data = $.extend( true, {}, this.formData, properties, items ),
+						summary = this.summary.getValue();
+					var apiData = {
+						action: 'forms-form-submit',
+						form: '_formEditor',
+						target: JSON.stringify( {
+							type: 'form-definition',
+							title: this.targetPage
+						} ),
+						data: JSON.stringify( data ),
+						token: mw.user.tokens.get( 'csrfToken' )
+					};
+					if ( summary ) {
+						apiData.summary = summary;
+					}
+					var api = new mw.Api();
+					api.post( apiData ).done( function( data ) {
+						if ( data.success === 1 ) {
+							mainPromise.resolve( this.close( { action: 'save', data: data} ) );
+						} else {
+							mainPromise.reject();
+						}
+					}.bind( this ) ).fail( function( code, data ) {
+						this.popPending();
+						mainPromise.reject();
+					}.bind( this ) );
+				}.bind ( this ) );
+
+				return mainPromise.promise();
+			}
+			if ( action === 'cancel' ) {
+				this.close( { action: 'cancel' } );
+			}
+		}, this
+	);
+};
--- resources/widget/editor/ElementPicker.js
+++ resources/widget/editor/ElementPicker.js
@@ -1,119 +1,62 @@
 ( function ( mw, $, undefined ) {
-	mw.ext.forms.widget.ElementPicker = function( options, forceOpen ) {
+	mw.ext.forms.widget.ElementPicker = function( options ) {
 		mw.ext.forms.widget.ElementPicker.super.call( this, {} );
 
-		this.forceOpen = forceOpen;
-		this.$trigger = this.makeTrigger();
-		this.$trigger.on( 'click', function() {
-			this.open();
-		}.bind( this ) );
 		this.$items = this.makeElements( options );
 
 		this.$element
 			.addClass( 'ext-forms-form-editor-element-picker' )
-			.append( this.$trigger, this.$items );
-
-		if ( forceOpen ) {
-			this.open( true );
-		} else {
-			this.close( true );
-		}
+			.append( this.$items );
 	};
 
 	OO.inheritClass( mw.ext.forms.widget.ElementPicker , OO.ui.Widget );
 
-	mw.ext.forms.widget.ElementPicker.prototype.makeTrigger = function() {
-		var $el = $( '<div>' ).addClass( 'picker-trigger' ),
-			icon = new OO.ui.IconWidget( { icon: 'outline' } ),
-			label = new OO.ui.LabelWidget( { label: 'Element picker' } ),
-			expandIcon = new OO.ui.IconWidget( { icon: 'expand' } );
-
-		$el.append( icon.$element, label.$element, expandIcon.$element );
-
-		return $el;
-	};
-
 	mw.ext.forms.widget.ElementPicker.prototype.makeElements = function( options ) {
-		this.back = new OO.ui.ButtonWidget( {
-			icon: 'close',
-			title: 'Close',
-			framed: false,
-			classes: [ 'close-elements' ]
-		} );
-
-		var $el = $( '<div>' ).addClass( 'picker-elements' ),
-			top = new OO.ui.HorizontalLayout( { items: [
-				new OO.ui.LabelWidget( {
-					label: 'Drag elements to the form below'
-				} ),
-				this.back
-			] } ),
-			items = new OO.ui.HorizontalLayout( {
-				classes: [ 'element-picker-items-layout' ]
-			} );
-
-		this.back.connect( this, {
-			click: 'close'
-		} );
-		$el.append( top.$element );
-
 		var sortedGroups = Object.keys( options ).sort();
+		var pages = [];
 		for ( var i = 0; i < sortedGroups.length; i++ ) {
 			var group = sortedGroups[i],
-				groupData = options[group],
-				layout = new OO.ui.FieldsetLayout( {
-					icon: groupData.group.getIcon() || '',
-					label: groupData.group.getDisplayText() || group
-				} );
+				groupData = options[group];
 
-			for( var ii = 0; ii < groupData.items.length; ii++ ) {
-				var item =  new mw.ext.forms.widget.ElementPickerItem ( {
-					label: groupData.items[ii].label,
-					icon: groupData.items[ii].icon,
-					data: {
-						key: groupData.items[ii].data,
-						type: groupData.items[ii].type
-					}
-				} );
-				layout.addItems( [ item ] );
+			if ( group === 'layout' ) {
+				// TODO: Layouts not working properly
+				continue;
 			}
-
-			items.addItems( [ layout ] );
+			pages.push( this.makePage( group, groupData ) );
 		}
+		var booklet = new OO.ui.BookletLayout( {
+			expanded: false,
+			outlined: true
+		} );
+		booklet.addPages( pages );
 
-		$el.append( items.$element );
-		return $el;
-	};
-
-	mw.ext.forms.widget.ElementPicker.prototype.dropDone = function() {
-		if ( !this.forceOpen ) {
-			this.close( true );
-		}
+		return $( '<div>' ).addClass( 'picker-elements' ).append( booklet.$element );
 	};
 
 	mw.ext.forms.widget.ElementPicker.prototype.dragStart = function() {
 
 	};
 
-	mw.ext.forms.widget.ElementPicker.prototype.open = function( instant ) {
-		instant = instant || false;
-		this.$trigger.hide();
-		if ( instant ) {
-			this.$items.show();
-		} else {
-			this.$items.slideDown();
+	mw.ext.forms.widget.ElementPicker.prototype.makePage = function( group, groupData ) {
+		var page = new OO.ui.PageLayout( group, {
+			expanded: false, classes: ['ext-forms-form-editor-element-picker-page']
+		} );
+		page.setupOutlineItem = function() {
+			this.outlineItem.setLabel( groupData.group.getDisplayText() || group );
+			this.outlineItem.setIcon( groupData.group.getIcon() || '' );
+		};
+		for( var i = 0; i < groupData.items.length; i++ ) {
+			var item = new mw.ext.forms.widget.ElementPickerItem ( {
+				label: groupData.items[i].label,
+				icon: groupData.items[i].icon,
+				data: {
+					key: groupData.items[i].data,
+					type: groupData.items[i].type
+				}
+			} );
+			page.$element.append( item.$element );
 		}
-	};
+		return page;
 
-	mw.ext.forms.widget.ElementPicker.prototype.close = function( instant ) {
-		instant = instant || false;
-		if ( instant ) {
-			this.$items.hide();
-			this.$trigger.show();
-		} else {
-			this.$items.slideUp( 400, function () {
-				this.$trigger.slideDown();
-			}.bind( this ) );
-		}
 	};
 } )( mediaWiki, jQuery );
