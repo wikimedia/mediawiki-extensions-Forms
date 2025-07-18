67356e93b4fb9434c29a78ab2ddc6b8344810e46
d.savuljesku <d.savuljesku@gmail.com>
2025-07-18 15:40:38 +0200

    Custom CategoryManipulator
    Depends-On: Ib8d8fa8065f8bb8b44c7bd31cfd1e4e1c765e7f4
    
    [5.1.x]?
    
    ERM44996
    
    Change-Id: I7638a5bc55f73c864f353a79b862ba48602e10e2
    (cherry picked from commit 99fc88176425902d2c3b84f1553ab1067fb5f856)
    (cherry picked from commit 438dd6cd8d768bd9049ecee65935723bff956ecc)

--- src/Content/FormDefinitionContent.php
+++ src/Content/FormDefinitionContent.php
@@ -24,4 +24,19 @@ class FormDefinitionContent extends FormDataContent {
 	protected function getPageFormat() {
 		return DefinitionManager::DEFINITION_PAGE_SUFFIX;
 	}
+
+	/**
+	 * @param array $categories
+	 * @return FormDefinitionContent|null
+	 */
+	public function setCategories( array $categories ): ?static {
+		if ( !$this->isValid() ) {
+			return null;
+		}
+		$values = json_decode( $this->mText, true );
+		$values['categories'] = $categories;
+		$this->mText = json_encode( $values, JSON_PRETTY_PRINT );
+		$this->jsonParse = null;
+		return $this;
+	}
 }
